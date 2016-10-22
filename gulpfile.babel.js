376121aea408dc32e3a2621cfe25d048f754c836
Mark Wise <m2r5.w4s3@gmail.com>
2016-10-22 18:27:12 -0500

    gulpfile cleanup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -30,40 +30,6 @@ paths.views =      `${paths.src}/views`;
 // html
 ////////////////////////////////////////////////////////////////////////////////
 
-// function html(src) {
-//     return gulp.src(src)
-//         .pipe(plugins.if(env === 'production',
-//             plugins.htmlmin({
-//                 collapseBooleanAttributes: true,
-//                 collapseWhitespace: true,
-//                 html5: true,
-//                 minifyCSS: true,
-//                 minifyJS: true,
-//                 removeAttributeQuotes: true,
-//                 removeComments: true,
-//                 removeEmptyAttributes: true,
-//                 removeScriptTypeAttributes: true,
-//                 removeStyleLinkTypeAttributes: true,
-//                 sortAttributes: true,
-//                 sortClassName: true,
-//                 useShortDoctype: true
-//             })
-//         ))
-//         .pipe(plugins.replace('@uuid', uuid))
-//         .pipe(gulp.dest(paths.dist));
-// };
-//
-// gulp.task('html:login', () => {
-//     return html([`${paths.login}/login.html`]);
-// });
-//
-// gulp.task('html:portal', () => {
-//     return html([`${paths.portal}/portal.html`]);
-// });
-//
-// gulp.task('html', ['html:login', 'html:portal']);
-
-
 gulp.task('html', () =>
     gulp.src(`${paths.src}/index.html`)
         .pipe(plugins.if(env === 'production',
@@ -88,54 +54,9 @@ gulp.task('html', () =>
 );
 
 
-
 ////////////////////////////////////////////////////////////////////////////////
 // styles
 ////////////////////////////////////////////////////////////////////////////////
-
-// function styles(appName, src) {
-//     return gulp.src(src)
-//         .pipe(plugins.sourcemaps.init())
-//             .pipe(plugins.concat(`${appName}${uuid}.css`))
-//             .pipe(plugins.less())
-//             .pipe(plugins.if(env === 'production', plugins.cssnano()))
-//         .pipe(plugins.sourcemaps.write('.'))
-//         .pipe(plugins.size({title: 'css'}))
-//         .pipe(gulp.dest(`${paths.static}/${appName}`));
-// };
-//
-// gulp.task('styles:login', () => {
-//     return styles('login', [
-//         `${paths.shared}/fontello/css/fontello-embedded.css`,
-//         `${paths.vendor}/normalize-css/normalize.css`,
-//         `${paths.styles}/mixins/**/*.less`,
-//         `${paths.styles}/colors.less`,
-//         `${paths.styles}/base.less`,
-//         `${paths.styles}/ui.less`,
-//         `${paths.components}/**/*.less`,
-//         `${paths.styles}/themes/ocs.less`,
-//         `${paths.login}/login.less`,
-//         `${paths.login}/views/**/*.{less,css}`
-//     ]);
-// });
-//
-// gulp.task('styles:portal', () => {
-//     return styles('portal', [
-//         `${paths.shared}/fontello/css/fontello-embedded.css`,
-//         `${paths.vendor}/normalize-css/normalize.css`,
-//         `${paths.styles}/mixins/**/*.less`,
-//         `${paths.styles}/colors.less`,
-//         `${paths.styles}/base.less`,
-//         `${paths.styles}/ui.less`,
-//         `${paths.components}/**/*.less`,
-//         `${paths.styles}/themes/ocs.less`,
-//         `${paths.portal}/portal.less`,
-//         `${paths.portal}/views/**/*.{less,css}`
-//     ]);
-// });
-//
-// gulp.task('styles', ['styles:login', 'styles:portal']);
-
 
 gulp.task('styles', () =>
     gulp.src([
@@ -156,32 +77,6 @@ gulp.task('styles', () =>
 ////////////////////////////////////////////////////////////////////////////////
 // scripts
 ////////////////////////////////////////////////////////////////////////////////
-
-// function scripts (appName, done) {
-//     let config = webpackConfig(appName, uuid);
-//
-//     webpack(config).run((err, stats) => {
-//         if (err) {
-//             done(err);
-//         } else {
-//             stats.compilation.fileDependencies.forEach((item) => {
-//                 //console.log(chalk.green(item));
-//             });
-//             done();
-//         }
-//     });
-// };
-//
-// gulp.task('scripts:login', (done) => {
-//     scripts('login', done);
-// });
-//
-// gulp.task('scripts:portal', (done) => {
-//     scripts('portal', done);
-// });
-//
-// gulp.task('scripts', ['scripts:login', 'scripts:portal']);
-
 
 gulp.task('scripts', (done) => {
     let config = webpackConfig(uuid);
