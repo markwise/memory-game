dba30707c1665c62c37d55b502d3b3cfae2b4287
Mark Wise <m2r5.w4s3@gmail.com>
2016-10-23 21:42:31 -0500

    rendering GuessCount in Main view

--- src/views/main/Main.js
+++ src/views/main/Main.js
@@ -1,6 +1,7 @@
 import Backbone from 'backbone';
 import Events from 'scripts/events';
 import CardManager from 'views/cardManager/CardManager';
+import GuessCount from 'views/guessCount/GuessCount';
 import template from './main.njk';
 
 
@@ -12,15 +13,19 @@ export default Backbone.View.extend({
     className: 'Main',
 
     initialize() {
-        this._cardManager = new CardManager({
-            cardDeck: cardDeck
-        });
+        this._cardManager = new CardManager({cardDeck: cardDeck});
+        this._guessCount = new GuessCount();
     },
 
     render() {
         let el = this.el;
         el.innerHTML = template.render();
         el.querySelector('.Main-content').appendChild(this._cardManager.el);
+        el.querySelector('.Main-status').appendChild(this._guessCount.render().el);
+
+
+
+
         Events.trigger('NEW_GAME');
     }
 });
